a1a364697e4340881f267469e5233a34de2ee8c2
Dev-Istyak <148707940+Dev-Istyak@users.noreply.github.com>
2025-04-12 23:18:44 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const choices=["rock","paper","scissor"];
+const playerDisplay=document.getElementById("playerdisplay");
+const ComputerDisplay=document.getElementById("computerdisplay");
+const DisplayResult=document.getElementById("Displayresult");
+const playerscoreDisplay=document.getElementById("playerscore");
+const computerscoreDisplay=document.getElementById("computerscore");
+let playerscore=0;
+let computerscore=0;
+
+function playgames(playerChoice){
+    const computerChoice= choices[Math.floor(Math.random()*3)];
+    let result="";
+
+    if(playerChoice===computerChoice){
+        result="It's A TIE!";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result=(computerChoice=== "scissor")?"YOU WIN":"YOU LOSE";
+                break;
+            case "paper":
+                result=(computerChoice === "rock")?"YOU WIN":"YOU LOSE";
+                break;
+             case "scissor":
+                result=(computerChoice ==="paper")?"YOU WIN":"YOU LOSE";
+                break;
+        }
+    }
+
+    playerDisplay.textContent=`PLAYER: ${playerChoice}`
+    ComputerDisplay.textContent=`COMPUTER: ${computerChoice}`
+    DisplayResult.textContent=result;
+    DisplayResult.classList.remove("greencolor","redcolor")
+
+    switch(result){
+        case "YOU WIN":
+            DisplayResult.classList.add("greencolor");
+            playerscore++;
+            playerscoreDisplay.textContent=playerscore;
+            break;
+        case "YOU LOSE":
+            DisplayResult.classList.add("redcolor");
+            computerscore++;
+            computerscoreDisplay.textContent=computerscore;
+    }
+}
